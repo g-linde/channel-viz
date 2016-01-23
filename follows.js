c42647d32c638e49da05c19a4a805d44eed841c1
g-linde <greg@thelindefamily.net>
2016-01-23 11:31:46 -0500

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'LYJOht2aSvQiWOaMpgpltYv9F42l0u2IrmBywGnNpzDFIdDO', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [641990755], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Pi', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
